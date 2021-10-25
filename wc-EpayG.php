172a16239314434b7cc8227200757b03dd01604c
heymmar-zeledon <Eliazithzeledon27@gmail.com>
2021-10-25 10:36:17 -0600

    Añadiendo informacion del plugin

---
+++ wc-EpayG.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin name: EpayG
+Plugin URI: https://github.com/heymmar-zeledon/EpayG
+Description: Extension de WooCommerce añadiendo una pasarela de pago
+Version: 1.0
+Author: Heymmar_Zeledon
+Author URI: https://github.com/heymmar-zeledon
+*/
+?>
