ba6afae04499604f79bec9642b97a1fb29524a90
heymmar-zeledon <Eliazithzeledon27@gmail.com>
2021-10-29 01:08:03 -0600

    reestructurado de codigo + campos de configuracion visibles

---
+++ wc-EpayG-Payment-Gateway.php
@@ -0,0 +1,76 @@
+<?php
+//Creamos nuestra propia clase de pasarela heredando de WC_Payment_Gateway
+class WC_EpayG_Payment_Gateway extends WC_Payment_Gateway
+{
+  function __construct() {
+    //ID global para nuestro metodo de pago
+    $this->id = "EpayG_payment";
+
+    // Titulo que se mostrara en la parte superior de la pagina de pasarelas de pago
+    $this->method_title = __( "EPAYG PAYMENT GATEWAY", 'EpayG_payment' );
+
+    // Descripcion de la pasarela de pago mostrada en la pagina de opciones de pago real
+    $this->method_description = __( "EPAYG Payment Gateway Plug-in for WooCommerce", 'EpayG_payment' );
+
+    // Titulo que se utilizara para las pestañas verticales que se pueden ordenar de arriba a abajo
+    $this->title = __( "EPAYG Payment Gateway", 'EpayG_payment' );
+
+    // Si se desea mostrar un icono o no de la pasarela de pago
+    $this->icon = null;
+
+    // integracion directa para los campos de pago se muestren en en proceso de pago.
+    $this->has_fields = true;
+
+    // Formulario de tarjeta de credito por defecto
+    $this->supports = array( 'default_credit_card_form' );
+
+    // Definicion de su configuracion y son cargados con init_settings()
+    $this->init_form_fields();
+
+    //obtenemos la configuracion y podemos cargarlas en variables
+    $this->init_settings();
+
+    //convertimos las variables para poder usarlas
+    foreach ( $this->settings as $setting_key => $value ) {
+       $this->$setting_key = $value;
+    }
+
+      //chequeamos el SSL
+    add_action( 'admin_notices', array( $this,  'do_ssl_check' ) );
+
+    // Guardamos la configuracion
+    if ( is_admin() ) {
+      add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+    }
+  } // Fin del constructor()
+
+  //Campos de configuracion del plugin
+  public function init_form_fields() {
+    $this->form_fields = array(
+      'enabled' => array(
+        'title'   => __( 'Activar / Desactivar', 'EpayG-payment' ),
+        'label'   => __( 'Activar el metodo de pago EpayG', 'EpayG-payment' ),
+        'type'    => 'checkbox',
+        'default' => 'no',
+      ),
+      'title' => array(
+        'title'   => __( 'Título', 'EpayG-payment' ),
+        'type'    => 'text',
+        'desc_tip'  => __( 'Título de pago que el cliente verá durante el proceso de pago.', 'EpayG-payment' ),
+        'default' => __( 'Tarjeta de crédito', 'bac-payment' ),
+      ),
+      'description' => array(
+        'title'   => __( 'Descripción', 'EpayG-payment' ),
+        'type'    => 'textarea',
+        'desc_tip'  => __( 'Descripción de pago que el cliente verá durante el proceso de pago.', 'EpayG-payment' ),
+        'default' => __( 'Pague con seguridad usando su tarjeta de crédito.', 'EpayG-payment' ),
+        'css'   => 'max-width:350px;'
+      ),
+    );
+  }
+
+
+
+
+
+?>
--- wc-EpayG.php
+++ wc-EpayG.php
@@ -17,54 +17,15 @@ add_action('plugins_loaded','EpayG_payment_init', 11)
 function EpayG_payment_init()
 {
   //Verificamos si la clase WC_Payment_Gateway existe
-  if(class_exists( WC_Payment_Gateway ))
-  {
-    //Creamos nuestra propia clase de pasarela heredando de WC_Payment_Gateway
-    class WC_EpayG_Payment_Gateway extends WC_Payment_Gateway
-    {
-      function __construct() {
-        //ID global para nuestro metodo de pago
-        $this->id = "EpayG_payment";
+  if(!class_exists( WC_Payment_Gateway ))return;
 
-        // Titulo que se mostrara en la parte superior de la pagina de pasarelas de pago
-        $this->method_title = __( "EPAYG PAYMENT GATEWAY", 'EpayG_payment' );
+  //incluimos el archivo principal de la pasarela
+  include_once("wc-EpayG-Payment_Gateway");
 
-        // Descripcion de la pasarela de pago mostrada en la pagina de opciones de pago real
-        $this->method_description = __( "EPAYG Payment Gateway Plug-in for WooCommerce", 'EpayG_payment' );
-
-        // Titulo que se utilizara para las pestañas verticales que se pueden ordenar de arriba a abajo
-        $this->title = __( "EPAYG Payment Gateway", 'EpayG_payment' );
-
-        // Si se desea mostrar un icono o no de la pasarela de pago
-        $this->icon = null;
-
-        // integracion directa para los campos de pago se muestren en en proceso de pago.
-        $this->has_fields = true;
-
-        // Formulario de tarjeta de credito por defecto
-        $this->supports = array( 'default_credit_card_form' );
-
-        // Definicion de su configuracion y son cargados con init_settings()
-        $this->init_form_fields();
-
-        //obtenemos la configuracion y podemos cargarlas en variables
-        $this->init_settings();
-
-        //convertimos las variables para poder usarlas
-        foreach ( $this->settings as $setting_key => $value ) {
-           $this->$setting_key = $value;
-        }
-
-          //chequeamos el SSL
-        add_action( 'admin_notices', array( $this,  'do_ssl_check' ) );
-
-        // Guardamos la configuracion
-        if ( is_admin() ) {
-          add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-        }
-      } // Fin del constructor()
-    }
+  //añadimos nuestro metodo de pago en WooCommerce
+  function add_bac_payment_gateway( $methods ){
+    $methods[]= 'EpayG_Payment_Gateway';
+    return $methods;
   }
 }
-
 ?>
