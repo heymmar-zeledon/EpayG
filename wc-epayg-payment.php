c5cca451987c9cc51d221b471a3fd3aaa88bd89d
fernando-cordoba <cordobaf290615@gmail.com>
2021-11-17 11:41:15 -0600

    comentando el codigo

--- wc-epayg-payment.php
+++ wc-epayg-payment.php
@@ -99,11 +99,13 @@ class EpayG_Payment_Gateway extends WC_Payment_Gateway {
 
     $time = time();
 
+    //obtenemos el key id configurado en nuestro plugin
     $key_id = $this->key_id;
 
-
+    //Reemplazamos el signo # con una cadena vacia "" para obtener el numero de orden puro
     $orderid = str_replace( "#", "", $customer_order->get_order_number() );
 
+    //construccion del hash de entrada
     $hash = md5($orderid."|".$customer_order->order_total."|".$time."|".$this->api_key);
 
     // Preparamos la informacion a enviar
@@ -122,11 +124,14 @@ class EpayG_Payment_Gateway extends WC_Payment_Gateway {
     // Enviamos esta autorizacion para el procesamiento
     $response = wp_remote_post( $url, array(
       'method'    => 'POST',
+      //http buils query lo utilizamos para crear una consulta de tipo http de modo que usara el hash de entrada y contruira una url respectiva
+      //Ejemplo de construccion: foo=bar&x=1&...n campos del hash
       'body'      => http_build_query( $payload ),
       'timeout'   => 90,
       'sslverify' => false,
     ) );
-
+    
+    //verificamos la respuesta obtenida si tiene algun error
     if ( is_wp_error( $response ) )
       throw new Exception( __( 'Ups! Tenemos un pequeño inconveniente con este pago, sentimos las molestias.', 'epayg_payment' ) );
 
@@ -140,8 +145,8 @@ class EpayG_Payment_Gateway extends WC_Payment_Gateway {
     $resp_e = explode( "&", $response_body ); //Convertimos el cuerpo de la respuesta en strings quitando el delimitador &
     $resp = array();
     foreach($resp_e as $r) {
-      $v = explode('=', $r);//separamos los string de cada iteracion del delimitador =
-      $resp[$v[0]] = $v[1];//Almacenamos los datos separados en el arreglo resp
+      $v = explode('=', $r); //separamos los string de cada iteracion del delimitador =
+      $resp[$v[0]] = $v[1]; //Almacenamos los datos separados en el arreglo resp
     }
 
     //Evaluamos la respuesta del codigo enviado para verificar si fue exitoso o no
@@ -149,7 +154,7 @@ class EpayG_Payment_Gateway extends WC_Payment_Gateway {
       // El pago se completo con exito
       $customer_order->add_order_note( __( 'Pago completado con exito.', 'epayg_payment' ) );
 
-      // Guardando la informacion
+      // Guardando la informacion de los metacampos de nuestra tienda
       $order_id = method_exists( $customer_order, 'get_id' ) ? $customer_order->get_id() : $customer_order->ID;
       update_post_meta($order_id , '_wc_order_authcode', $resp['authcode'] );
 			update_post_meta($order_id , '_wc_order_transactionid', $resp['transactionid'] );
@@ -172,7 +177,7 @@ class EpayG_Payment_Gateway extends WC_Payment_Gateway {
       $customer_order->add_order_note( 'Error: '. $resp['responsetext'] );
     }
 
-  }//fin del proceso de pago
+  } //fin del proceso de pago
 
   //funcion para validar los campos
   public function validate_fields() {
@@ -181,7 +186,7 @@ class EpayG_Payment_Gateway extends WC_Payment_Gateway {
 
 }
 
-//Mostramos el valor del campo en la página de edición del pedido
+//Mostramos la informacion de facturacion del pedido realizado
 add_action( 'woocommerce_admin_order_data_after_billing_address', 'show_info', 10, 1 );
 function show_info( $order ){
     $order_id = method_exists( $order, 'get_id' ) ? $order->get_id() : $order->id;
